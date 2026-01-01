96fb2275870dca0b3c028ca10bb45efa6de61c65
pravin-more <pravinmoreone@gmail.com>
2026-01-01 07:00:21 +0530

    commint 2

--- Implementing the _v and _t Pattern.cpp
+++ Implementing the _v and _t Pattern.cpp
@@ -1,4 +1,34 @@
-//
-// Created by PravinMore on 31/12/2025.
-//\
+#include <iostream>
+#include <type_traits>
 
+template <typename>
+struct is_renderable : std::false_type {};
+
+template <typename T>
+constexpr bool is_renderable_v{
+    is_renderable<T>::value};
+
+class Fish {
+public:
+    void Render() {
+        std::cout << "Fish: ><((((`>";
+    }
+};
+
+template <>
+struct is_renderable<Fish> : std::true_type {};
+
+template <typename T>
+void Render(T Param) {
+    if constexpr (is_renderable_v<T>) {
+        Param.Render();
+    } else {
+        std::cout << "\nNot Renderable";
+    }
+}
+
+int main() {
+    Fish MyFish;
+    Render(MyFish); // Renderable
+    Render(42);     // Not Renderable
+}
